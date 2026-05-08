ad85ea8b0cf056e4a7518cd495775562c828ab5f
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2026-05-08 00:01:15 -0300

    303 adequar apresentacao para versao mobile (#313)
    * Adicionados ajustes para versao mobile
    
    * Fonte ajustada para versoes mobile e intermediaria

--- src/views/PresentationLetterView.vue
+++ src/views/PresentationLetterView.vue
@@ -85,7 +85,7 @@ import ScrollablePage from '@/components/ScrollablePage.vue';
 }
 
 .presentation-title {
-  font-size: 4rem;
+  font-size: 4.6vw;
   margin-bottom: 2.3rem;
   font-weight: 400;
   text-transform: uppercase;
@@ -106,11 +106,11 @@ import ScrollablePage from '@/components/ScrollablePage.vue';
 
 @media (max-width: 1024px) {
   .presentation-content {
-    padding: 1em 5vw;
+    padding: 1em 0;
   }
 
   .presentation-title {
-    font-size: 2.5rem;
+    font-size: 7vw;
   }
 }
 </style>
