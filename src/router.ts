80848da77381dc3ad8c4880b65014fbd41da384d
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2026-05-19 19:00:48 -0300

    Rota adicionada

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ import GeneroView from '@/views/GeneroView.vue'
 import GeneroMobileView from '@/views/GeneroMobileView.vue'
 import SaudeView from '@/views/SaudeView.vue'
 import UrbanismoView from '@/views/UrbanismoView.vue'
+import UrbanismoMobileView from '@/views/UrbanismoMobileView.vue'
 import OrcamentoView from '@/views/OrcamentoView.vue'
 import GlossaryView from '@/views/GlossaryView.vue'
 import NotFound from './views/NotFoundView.vue'
@@ -77,6 +78,12 @@ const router = createRouter({
       name: 'urbanismo',
       component: UrbanismoView,
       meta: { title: 'Urbanismo – DataSP' }
+    },
+    {
+      path: '/urbanismo-mobile',
+      name: 'urbanismo-mobile',
+      component: UrbanismoMobileView,
+      meta: { title: 'Urbanismo Mobile – DataSP' }
     },
     {
       path: '/orcamento',
