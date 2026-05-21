f5a6c4844bf3c2e65c09ff82c8158e35c01696fd
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2026-05-21 00:12:18 -0300

    320 remover aviso de incompatibilidade apos data de lancamento (#336)
    * Adicionada condição de exibição de alerta baseada na data
    
    * Adicionada condição de acesso antecipado

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,30 @@ import MobileWarning from './components/MobileWarning.vue'
 const isMobileOrPortrait = ref(false)
 
 const checkViewport = () => {
+  // Modo de acesso antecipado
+
+  if (new URLSearchParams(window.location.search).get('mobile_preview') === 'true') {
+    localStorage.setItem('mobilePreview', 'true')
+  }
+
+  const mobilePreview = localStorage.getItem('mobilePreview') === 'true'
+
+  isMobileOrPortrait.value = false
+
+  if (mobilePreview) {
+    return
+  }
+  
   const width = window.innerWidth
   const height = window.innerHeight
   
   // Exibe aviso se:
   // 1. Largura menor que altura (orientação retrato)
   // 2. Largura menor que 900px (dispositivos móveis típicos)
-  // Exceto se a URL contiver a palavra "mobile"
-  isMobileOrPortrait.value = false
-  if (!window.location.href.toLowerCase().includes('mobile')) {
+  // Exceto se a data for posterior ao lançamento (após 2026-06-18)
+  const currentDate = new Date()
+  const launchDate = new Date('2026-06-18')
+  if (currentDate < launchDate) {
     isMobileOrPortrait.value = width < height || width < 900
   }
 }
